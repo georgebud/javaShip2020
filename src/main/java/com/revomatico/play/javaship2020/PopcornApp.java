ffac44a3121940570c7f843c7bb7b1bcab46f8e8
vlad <vlad@example.com>
2020-07-09 18:52:34 +0100

    A little update(correct displayed movides:)) ) watch mvn test

--- src/main/java/com/revomatico/play/javaship2020/PopcornApp.java
+++ src/main/java/com/revomatico/play/javaship2020/PopcornApp.java
@@ -42,17 +42,16 @@ public class PopcornApp {
       boolean swapped;
       for(int i=0; i< movies.size()-1; i++)
       {
-        swapped=false;
+        
         for(int j=0;j<movies.size()-i-1;j++)
         {
-          if(movies.get(i).getDate()>movies.get(j+1).getDate())
+          if(movies.get(j).getDate()>movies.get(j+1).getDate())
           {
             Collections.swap(movies, j, j+1); 
-            swapped=true;
+            
           }
         }
-          if(swapped=false)
-            break;
+          
       }
       System.out.println("Sorted movies are: ");
       for(int i=0;i<movies.size();i++)
