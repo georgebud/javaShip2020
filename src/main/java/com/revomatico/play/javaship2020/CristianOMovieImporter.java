5872ed8b8b641d8a28c80a03a23ddaea1703be6b
looneytunes299 <cristian.ocheana@yahoo.com>
2020-07-15 00:47:58 +0300

    refactor importMovie implementation / create getIndex method

--- src/main/java/com/revomatico/play/javaship2020/CristianOMovieImporter.java
+++ src/main/java/com/revomatico/play/javaship2020/CristianOMovieImporter.java
@@ -2,16 +2,19 @@ package com.revomatico.play.javaship2020;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
 
 import java.io.FileReader;
-import java.lang.reflect.Array;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 public class CristianOMovieImporter implements MovieImporter {
-
+    private static final String YEAR = "year";
+    private static final String TITLE = "title";
 
     public static void main(String[] args) {
         String path = "src\\main\\resources\\WATCHLIST.csv";
@@ -27,7 +30,6 @@ public class CristianOMovieImporter implements MovieImporter {
     @Override
     public List<Movie> importMovies(String path) {
 
-
         List<Movie> movieList = new ArrayList<>();
 
         try {
@@ -37,18 +39,49 @@ public class CristianOMovieImporter implements MovieImporter {
             CSVReader csvReader = new CSVReaderBuilder(filereader)
                     .withSkipLines(1)
                     .build();
-
             SimpleDateFormat format = new SimpleDateFormat("yyyy");
             List<String[]> allData = csvReader.readAll();
 
+            String title = null;
+            Date productionDate = null;
+
             for (String[] row : allData) {
-                String title = String.valueOf(Array.get(row, 5));
-                Date productionDate = format.parse(String.valueOf(Array.get(row, 10)));
+                for (String cell : row) {
+                    if (getIndex(path, TITLE) == Arrays.asList(row).indexOf(cell)) {
+                        title = cell;
+                    }
+                    if (getIndex(path, YEAR) == Arrays.asList(row).indexOf(cell)) {
+                        productionDate = format.parse(cell);
+                    }
+                }
                 movieList.add(new Movie(title, productionDate));
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         return movieList;
+    }
+
+    public int getIndex(String path, String field) {
+        int result = 0;
+
+        try (FileReader reader = new FileReader(path)){
+
+            CSVReader csvReader = new CSVReader(reader);
+            String[] nextData;
+
+            while ((nextData = csvReader.readNext()) != null) {
+                for (String cell : nextData) {
+                    if (cell.equalsIgnoreCase(field)) {
+                        result = Arrays.asList(nextData).indexOf(cell);
+                    }
+                }
+            }
+
+        } catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
