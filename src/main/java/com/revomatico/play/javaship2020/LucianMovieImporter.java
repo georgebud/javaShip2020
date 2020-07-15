c6bd16006fd510382b1c456858bfbbc6b37ffd86
lucian606 <54243052+lucian606@users.noreply.github.com>
2020-07-15 15:39:27 +0300

    Add files via upload

--- src/main/java/com/revomatico/play/javaship2020/LucianMovieImporter.java
+++ src/main/java/com/revomatico/play/javaship2020/LucianMovieImporter.java
@@ -11,159 +11,139 @@ import java.util.List;
 import java.util.Scanner;
 
 public class LucianMovieImporter implements MovieImporter {
-  class Movie implements Comparable<Movie> {
-    private String title;
-    private float rating;
-    private Date release_date;
 
-    public Movie(String title, float rating, Date release_date) {
-      this.title = title;
-      this.rating = rating;
-      this.release_date = release_date;
-    }
+    static class CSVUtils {
 
-    @Override
-    public int compareTo(Movie m) {
-      if (release_date.after(m.release_date)) {
-        return 1;
-      } else if (release_date.before(m.release_date)) {
-        return -1;
-      } else {
-        return title.compareTo(m.title);
-      }
-    }
+        private static final char DEFAULT_SEPARATOR = ',';
+        private static final char DEFAULT_QUOTE = '"';
 
-    @Override
-    public String toString() {
-      return "Title: " + title + " Rating: " + rating + " Release date: " + release_date + "\n";
-    }
-  }
+        public static List<String> parseLine(String cvsLine) {
+            return parseLine(cvsLine, DEFAULT_SEPARATOR, DEFAULT_QUOTE);
+        }
 
-  static class CSVUtils {
+        public static List<String> parseLine(String cvsLine, char separators) {
+            return parseLine(cvsLine, separators, DEFAULT_QUOTE);
+        }
 
-    private static final char DEFAULT_SEPARATOR = ',';
-    private static final char DEFAULT_QUOTE = '"';
+        public static List<String> parseLine(String cvsLine, char separators, char customQuote) {
 
-    public static List<String> parseLine(String cvsLine) {
-      return parseLine(cvsLine, DEFAULT_SEPARATOR, DEFAULT_QUOTE);
-    }
+            List<String> result = new ArrayList<>();
 
-    public static List<String> parseLine(String cvsLine, char separators) {
-      return parseLine(cvsLine, separators, DEFAULT_QUOTE);
-    }
-
-    public static List<String> parseLine(String cvsLine, char separators, char customQuote) {
-
-      List<String> result = new ArrayList<>();
-
-      //if empty, return!
-      if (cvsLine == null || cvsLine.isEmpty()) {
-        return result;
-      }
-
-      if (customQuote == ' ') {
-        customQuote = DEFAULT_QUOTE;
-      }
-
-      if (separators == ' ') {
-        separators = DEFAULT_SEPARATOR;
-      }
-
-      StringBuffer curVal = new StringBuffer();
-      boolean inQuotes = false;
-      boolean startCollectChar = false;
-      boolean doubleQuotesInColumn = false;
-
-      char[] chars = cvsLine.toCharArray();
-
-      for (char ch : chars) {
-
-        if (inQuotes) {
-          startCollectChar = true;
-          if (ch == customQuote) {
-            inQuotes = false;
-            doubleQuotesInColumn = false;
-          } else {
-
-            //Fixed : allow "" in custom quote enclosed
-            if (ch == '\"') {
-              if (!doubleQuotesInColumn) {
-                curVal.append(ch);
-                doubleQuotesInColumn = true;
-              }
-            } else {
-              curVal.append(ch);
+            //if empty, return!
+            if (cvsLine == null || cvsLine.isEmpty()) {
+                return result;
             }
 
-          }
-        } else {
-          if (ch == customQuote) {
-
-            inQuotes = true;
-
-            //Fixed : allow "" in empty quote enclosed
-            if (chars[0] != '"' && customQuote == '\"') {
-              curVal.append('"');
+            if (customQuote == ' ') {
+                customQuote = DEFAULT_QUOTE;
             }
 
-            //double quotes in column will hit this!
-            if (startCollectChar) {
-              curVal.append('"');
+            if (separators == ' ') {
+                separators = DEFAULT_SEPARATOR;
             }
 
-          } else if (ch == separators) {
+            StringBuffer curVal = new StringBuffer();
+            boolean inQuotes = false;
+            boolean startCollectChar = false;
+            boolean doubleQuotesInColumn = false;
+
+            char[] chars = cvsLine.toCharArray();
+
+            for (char ch : chars) {
+
+                if (inQuotes) {
+                    startCollectChar = true;
+                    if (ch == customQuote) {
+                        inQuotes = false;
+                        doubleQuotesInColumn = false;
+                    } else {
+
+                        //Fixed : allow "" in custom quote enclosed
+                        if (ch == '\"') {
+                            if (!doubleQuotesInColumn) {
+                                curVal.append(ch);
+                                doubleQuotesInColumn = true;
+                            }
+                        } else {
+                            curVal.append(ch);
+                        }
+
+                    }
+                } else {
+                    if (ch == customQuote) {
+
+                        inQuotes = true;
+
+                        //Fixed : allow "" in empty quote enclosed
+                        if (chars[0] != '"' && customQuote == '\"') {
+                            curVal.append('"');
+                        }
+
+                        //double quotes in column will hit this!
+                        if (startCollectChar) {
+                            curVal.append('"');
+                        }
+
+                    } else if (ch == separators) {
+
+                        result.add(curVal.toString());
+
+                        curVal = new StringBuffer();
+                        startCollectChar = false;
+
+                    } else if (ch == '\r') {
+                        //ignore LF characters
+                        continue;
+                    } else if (ch == '\n') {
+                        //the end, break!
+                        break;
+                    } else {
+                        curVal.append(ch);
+                    }
+                }
+
+            }
 
             result.add(curVal.toString());
 
-            curVal = new StringBuffer();
-            startCollectChar = false;
-
-          } else if (ch == '\r') {
-            //ignore LF characters
-            continue;
-          } else if (ch == '\n') {
-            //the end, break!
-            break;
-          } else {
-            curVal.append(ch);
-          }
+            return result;
         }
 
-      }
-
-      result.add(curVal.toString());
-
-      return result;
     }
 
-  }
+    public List<Movie> listMovies(String filePath) throws FileNotFoundException, ParseException {
+        Scanner scanner = new Scanner(new File(filePath));
+        scanner.nextLine(); //Skipping the first line, which contains the column names
+        List<Movie> movieList = new ArrayList<>();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd"); //Date format used in date parsing
 
-  public void listMovies(String filePath) throws FileNotFoundException, ParseException {
-    Scanner scanner = new Scanner(new File(filePath));
-    scanner.nextLine(); //Skipping the first line, which contains the column names
-    List<Movie> movieList = new ArrayList<>();
-    SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd"); //Date format used in date parsing
+        while (scanner.hasNext()) {
+            List<String> line = CSVUtils.parseLine(scanner.nextLine());
+            String title = line.get(5); //Getting the movie title
+            String ratingString = line.get(8); //Getting the movie's score
+            String dateString = line.get(13); //Getting the date of the movie
+            Date date = format.parse(dateString); //Formating the date from string to date
+            if (title.charAt(0) == '"') {
+                title = title.substring(1);
+            } //If the title was formated in case there was a coma in it we remove the " from the beginning
+            Movie current_movie = new Movie(title, date); //Creating the movie
+            movieList.add(current_movie); //Adding the movie to the list
+        }
 
-    while (scanner.hasNext()) {
-      List<String> line = CSVUtils.parseLine(scanner.nextLine());
-      String title = line.get(5); //Getting the movie title
-      String ratingString = line.get(8); //Getting the movie's score
-      float rating = Float.parseFloat(ratingString);
-      String dateString = line.get(13); //Getting the date of the movie
-      Date date = format.parse(dateString); //Formating the date from string to date
-      if (title.charAt(0) == '"') {
-        title = title.substring(1);
-      } //If the title was formated in case there was a coma in it we remove the " from the beginning
-      Movie current_movie = new Movie(title, rating, date); //Creating the movie
-      movieList.add(current_movie); //Adding the movie to the list
+        Collections.sort(movieList); //Sorting the movies
+        return movieList;
     }
 
-    Collections.sort(movieList); //Sorting the movies
-    System.out.println(movieList);
-  }
-
-  @Override
-  public List<com.revomatico.play.javaship2020.Movie> importMovies(String path) {
-    throw new RuntimeException("Not implemented yet!!!");
-  }
+    @Override
+    public List<Movie> importMovies(String path) {
+        try {
+            return listMovies(path);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("No file found! When trying to import movies from [" + path + "]",e);
+        } catch (ParseException e) {
+            throw new RuntimeException("When trying to parse movies from [" + path + "]",e);
+        } catch (Exception e) {
+            throw new RuntimeException("Exception! When trying to import movies from [" + path + "]",e);
+        }
+    }
 }
