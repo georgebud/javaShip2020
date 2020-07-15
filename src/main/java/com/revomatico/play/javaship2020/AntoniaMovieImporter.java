112458008204d025b737f0fa2816e4cc5837b63e
raisercostin <raisercostin@gmail.com>
2020-07-15 13:33:03 +0300

    fix resource leak

--- src/main/java/com/revomatico/play/javaship2020/AntoniaMovieImporter.java
+++ src/main/java/com/revomatico/play/javaship2020/AntoniaMovieImporter.java
@@ -12,39 +12,43 @@ import java.util.List;
 
 public class AntoniaMovieImporter implements MovieImporter {
 
-	@Override
-	public List<Movie> importMovies(String path) {
-		
-		List<Movie> moviesAntonia = new ArrayList<>();
-		String line = "";
-		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		   
-		try {
-			BufferedReader b = new BufferedReader(new FileReader(path));
+  @Override
+  public List<Movie> importMovies(String path) {
 
-			line = b.readLine();//get over columns' names
-			while((line = b.readLine()) != null ) {
-				
-				   String[] s = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");//instead of line.split(",");
-				   Movie movie = new Movie(s[5],dateFormat.parse(s[13])); //s[5]=title s[13]=releaseDate
-				   moviesAntonia.add(movie);		
-			}
+    List<Movie> moviesAntonia = new ArrayList<>();
+    String line = "";
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    BufferedReader b = null;
+    try {
+      b = new BufferedReader(new FileReader(path));
 
-		  } 
-		 catch (FileNotFoundException e) {
-			   System.out.println("Can not find the csv file");
-			   e.printStackTrace();
-		   } 
-		 catch (IOException e) {
-			   System.out.println("Can not open the csv file");
-			   e.printStackTrace();
-		   } 
-		 catch (ParseException e) {
-			   System.out.println("Can not create Date object");
-			   e.printStackTrace();
-		   }
-		   
-		return moviesAntonia;		   
-	} 
+      line = b.readLine();//get over columns' names
+      while ((line = b.readLine()) != null) {
+
+        String[] s = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");//instead of line.split(",");
+        Movie movie = new Movie(s[5], dateFormat.parse(s[13])); //s[5]=title s[13]=releaseDate
+        moviesAntonia.add(movie);
+      }
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException("When trying to importMovies from [" + path + "]", e);
+    } catch (IOException e) {
+      throw new RuntimeException("When trying to importMovies from [" + path + "]", e);
+    } catch (ParseException e) {
+      throw new RuntimeException("When trying to importMovies from [" + path + "]", e);
+    } finally {
+      if (b != null) {
+        try {
+          b.close();
+        } catch (IOException e) {
+          //log.debug("ignoring exception",e);
+          System.out.println("ignoring exception");
+          e.printStackTrace();
+          //          throw new RuntimeException("When trying to importMovies from [" + path + "]", e);
+        }
+      }
+    }
+
+    return moviesAntonia;
+  }
 
 }
