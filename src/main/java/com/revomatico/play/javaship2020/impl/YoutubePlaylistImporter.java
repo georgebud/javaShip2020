2ffcee5df45bdad52b51a656a005de994275f2da
Bogdan Dinescu <bogdanbogdan476@gmail.com>
2020-07-28 02:48:47 +0300

    Fix previous commit

--- src/main/java/com/revomatico/play/javaship2020/impl/YoutubePlaylistImporter.java
+++ src/main/java/com/revomatico/play/javaship2020/impl/YoutubePlaylistImporter.java
@@ -2,12 +2,30 @@ package com.revomatico.play.javaship2020.impl;
 
 import com.revomatico.play.javaship2020.MediaItem;
 import com.revomatico.play.javaship2020.MediaItemImporter;
+import io.vavr.collection.Seq;
+import org.raisercostin.jedio.Locations;
+import org.raisercostin.nodes.Nodes;
 
 import java.util.List;
 
 public class YoutubePlaylistImporter implements MediaItemImporter {
   @Override
   public List<MediaItem> importMediaItems(String path) {
-    return null;
+    String playlistId = "PL4o29bINVT4EG_y-k5jGoOu3-Am8Nvi10";
+    String content = Locations.url(String.format(
+      "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=%s&key=AIzaSyDaU9IH856cYXsNvoctljCzAStI8kbmGrI",
+      playlistId)).readContentSync();
+    YoutubePlaylist result = Nodes.json.withIgnoreUnknwon().toObject(content, YoutubePlaylist.class);
+    return result.items.map(youtubeItem -> youtubeItem.snippet)
+      .map(x -> x.validate())
+      .toJavaList();
   }
+}
+
+class YoutubePlaylist {
+  Seq<YoutubeItem> items;
+}
+
+class YoutubeItem {
+  MediaItem snippet;
 }
--- src/test/java/com/revomatico/play/javaship2020/impl/YoutubePlaylistImporterTest.java
+++ src/test/java/com/revomatico/play/javaship2020/impl/YoutubePlaylistImporterTest.java
@@ -1,4 +1,18 @@
-package com.revomatico.play.javaship2020.impl.samples;
+package com.revomatico.play.javaship2020.impl;
+
+import com.revomatico.play.javaship2020.MediaItem;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class YoutubePlaylistImporterTest {
+
+  @Test
+  void testImportMediaItems() {
+    List<MediaItem> list = new YoutubePlaylistImporter().importMediaItems("");
+    assertThat(list).isNotNull();
+    assertThat(list).hasSize(50);
+  }
 }
