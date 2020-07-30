aff11ba9a2ed1fd51ce552a32921bffc8c69e587
Anca Enache <ancamihaela91@yahoo.com>
2020-07-31 02:50:01 +0300

    Deprecated method and duplicated dependency removed.

--- src/main/java/com/revomatico/play/javaship2020/impl/AncaMovieImporter.java
+++ src/main/java/com/revomatico/play/javaship2020/impl/AncaMovieImporter.java
@@ -3,10 +3,10 @@ package com.revomatico.play.javaship2020.impl;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Scanner;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 import com.revomatico.play.javaship2020.MediaItem;
 import com.revomatico.play.javaship2020.MediaItemImporter;
@@ -16,16 +16,27 @@ public class AncaMovieImporter implements MediaItemImporter {
   @Override
   public List<MediaItem> importMediaItems(String path) {
     ArrayList<MediaItem> movies = new ArrayList<>();
+    DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
     try (Scanner file = new Scanner(new File(path), String.valueOf(StandardCharsets.UTF_8))) {
-      file.nextLine(); // name of the columns
+      String firstLine = file.nextLine();// name of the columns
+      String[] c = firstLine.split(",");
 
       while (file.hasNextLine()) {
         String line = file.nextLine();
         String[] split = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
-        movies.add(new MediaItem(split[5],
-          new Date(Long.parseLong(split[10]))));
+
+        HashMap<String, String> hashMap = new HashMap<>();
+        for (int i = 0; i < split.length; i++) {
+          hashMap.put(c[i], split[i]);
+        }
+
+        movies.add(new MediaItem(hashMap.get("Title"),
+          df.parse(hashMap.get("Release Date")),
+          hashMap.get("Image"), "",
+          hashMap.get("URL")));
       }
-    } catch (FileNotFoundException e) {
+
+    } catch (FileNotFoundException | ParseException e) {
       throw new RuntimeException("When trying to import movies from [" + path + "]", e);
     }
 
--- src/test/java/com/revomatico/play/javaship2020/impl/AncaTest.java
+++ src/test/java/com/revomatico/play/javaship2020/impl/AncaTest.java
@@ -1,14 +1,9 @@
 package com.revomatico.play.javaship2020.impl;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import java.util.List;
-
-import com.revomatico.play.javaship2020.MediaItem;
-import com.revomatico.play.javaship2020.MediaItemImporter;
 import com.revomatico.play.javaship2020.PopcornApp;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AncaTest {
   @Test
